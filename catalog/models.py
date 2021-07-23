779a14d42ece2554ae1343e297f0df0608d04a4e
Rahul <rahul.sharma201@outlook.com>
2021-07-23 17:35:41 +0530

    Create models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length = 20, help_text = 'Enter book genre (ex. Fiction)')
+
+    def __str__(self):
+        return self.name 
+
+class Book(models.Model):
+    title = models.CharField(max_length = 100)
+    author = models.ForeignKey('Author', on_delete = models.SET_NULL, null = True)
+    summary = models.TextField(max_length = 1000, help_text = "Enter description of book")
+    # International Standard Book Number. This 10 or 13-digit number.
+    isbn = models.CharField('ISBN', max_length = 13, unique = True, help_text = '10 or 13 digit Number')
+
+    genre = models.ManyToManyField(Genre, help_text = 'Select genre of book')
+
+    def __str__(self):
+        return self.title
+
+class BookInstance(models.Model):
+    #Universally unique identifiers are a good alternative to AutoField for primary_key. The database will not generate the UUID for you,
+    #so it is recommended to use default"
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4, help_text = "Unique ID of this particular book across library")
+
+    due_back = models.DateField(null = True, blank = True)
+    book = models.ForeignKey(Book, on_delete = models.RESTRICT, null = True)
+    imprint = models.CharField(max_length = 20)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved')
+    )
+
+    status = models.CharField(max_length = 1, choices = LOAN_STATUS, blank = True, default = 'm', help_text = "Book availability status")
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        return f'{self.id} ({self.book.title})'
+
+class Author(models.Model):
+    first_name = models.CharField(max_length = 100)
+    last_name = models.CharField(max_length = 100)
+    date_of_birth = models.DateField(null = True, blank = True)
+    date_of_death = models.DateField(null = True, blank = True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def __str__(self):
+        return f'{self.first_name}, {self.last_name}'
