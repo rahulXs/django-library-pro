e2cc427eb9c4ef1f4450b5420a6342ca6c1ca186
Rahul <rahul.sharma201@outlook.com>
2021-07-31 21:51:06 +0530

    test framework init and tests for models

--- catalog/models.py
+++ catalog/models.py
@@ -69,14 +69,14 @@ class Author(models.Model):
     first_name = models.CharField(max_length = 100)
     last_name = models.CharField(max_length = 100)
     date_of_birth = models.DateField(null = True, blank = True)
-    date_of_death = models.DateField(null = True, blank = True)
+    date_of_death = models.DateField('died', null = True, blank = True)
     #books = models.ForeignKey(Book, on_delete = models.SET_NULL)
 
     class Meta:
         ordering = ['last_name', 'first_name']
 
     def __str__(self):
-        return f'{self.first_name}, {self.last_name}'
+        return f'{self.last_name}, {self.first_name}'
 
     def get_absolute_url(self):
         """Returns the url to access a particular author instance."""
