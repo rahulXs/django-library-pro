62093a7f1f2bb69fabd3d7a96163cd413027ffaf
Rahul <rahul.sharma201@outlook.com>
2021-07-31 11:30:39 +0530

    Added generic editing views for author and book

--- catalog/urls.py
+++ catalog/urls.py
@@ -11,4 +11,11 @@ urlpatterns = [
     path(r'borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
 
     path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
+
+    path('book/create/', views.BookCreate.as_view(), name='book-create'),
+    path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book-update'),
+    path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book-delete'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -109,3 +109,36 @@ def renew_book_librarian(request, pk):
     }
 
     return render(request, 'catalog/book_renew_librarian.html', context)
+
+# generic editing views
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from catalog.models import Author
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    initial = {'date_of_death': '11/06/2020'}
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = '__all__' # Not recommended (potential security issue if more fields added)
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
+
+from catalog.models import Book 
+
+class BookCreate(CreateView):
+    model = Book 
+    fields = '__all__'
+
+class BookUpdate(UpdateView):
+    model = Book 
+    fields = '__all__' # Not recommended (potential security issue if more fields added)
+
+class BookDelete(DeleteView):
+    model = Book 
+    success_url = reverse_lazy('books')
